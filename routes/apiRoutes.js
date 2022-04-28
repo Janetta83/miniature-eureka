d158d131affc2666daf6344b9b72310b522e37b0
janetta <janetta_turner@yahoo.com>
2022-04-27 22:08:00 -0500

    trying to link files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const fs = require('fs');
+const savedNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+
+
+
+
+router.get('/notes/', (req, res) => {
+  res.json(savedNotes);
+  // res.json(savedNotes[req.params.id])
+});
+
+
+
+router.post('/notes', (req, res) => {
+    let pushedNotes = req.body;
+    savedNotes.push(pushedNotes);
+    console.log('Saved!')
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+
+
+module.exports = router;
